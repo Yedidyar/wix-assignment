204748152cbab5400302e2b1890aa27c946f126d
yedidya rashi <Yedidyarashi@gmail.com>
2023-07-04 17:59:06 +0300

    feat(tickets-service): add findAll endpoint

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -1,16 +1,14 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TicketsService } from './tickets.service';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('tickets')
 @Controller('tickets')
 export class TicketsController {
   constructor(private readonly ticketsService: TicketsService) {}
 
   @Get()
-  findAll(
-    @Query('title') title: string,
-    @Query('content') content: string,
-    @Query('email') email: string,
-  ) {
+  findAll() {
     return this.ticketsService.findAll();
   }
 }
--- src/tickets/tickets.service.ts
+++ src/tickets/tickets.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Ticket } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class TicketsService {
   constructor(private prisma: PrismaService) {}
 
-  findAll() {
-    this.prisma.ticket.findMany();
+  findAll(): Promise<Ticket[]> {
+    return this.prisma.ticket.findMany();
   }
 }
