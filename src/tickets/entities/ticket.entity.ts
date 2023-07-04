6d40b1a0712104dd5b8d5147d8a59ff702399b6d
yedidya rashi <Yedidyarashi@gmail.com>
2023-07-04 19:15:19 +0300

    fix(typo): lables to labels

--- src/tickets/entities/ticket.entity.ts
+++ src/tickets/entities/ticket.entity.ts
@@ -1,1 +1,10 @@
-export class Ticket {}
+import { Ticket as TicketModel } from '@prisma/client';
+
+export class Ticket implements TicketModel {
+  id: string;
+  title: string;
+  content: string;
+  userEmail: string;
+  creationTime: Date;
+  labels: string[];
+}
