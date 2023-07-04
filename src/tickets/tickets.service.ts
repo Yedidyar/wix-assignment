dd0559a95b895e766e723292ef19a7f2e75535ae
yedidya rashi <Yedidyarashi@gmail.com>
2023-07-04 20:19:44 +0300

    fix(tickets-service): handle when queryParamsDto optional

--- src/tickets/tickets.service.ts
+++ src/tickets/tickets.service.ts
@@ -7,19 +7,14 @@ import { QueryParamsDto } from './dto/query-params.dto';
 export class TicketsService {
   constructor(private prisma: PrismaService) {}
 
-  findAll({
-    creation_time_from,
-    creation_time_to,
-    email,
-    ...rest
-  }: QueryParamsDto): Promise<Ticket[]> {
+  findAll(queryParamsDto: QueryParamsDto): Promise<Ticket[]> {
     return this.prisma.ticket.findMany({
       where: {
-        ...rest,
-        userEmail: email,
+        content: queryParamsDto?.content,
+        userEmail: queryParamsDto?.email,
         creationTime: {
-          gte: creation_time_from,
-          lte: creation_time_to,
+          gte: queryParamsDto?.creation_time_from,
+          lte: queryParamsDto?.creation_time_to,
         },
       },
     });
